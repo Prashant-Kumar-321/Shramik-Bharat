00996f79e8df648065754de74263a5b9c068c13d
pk123 <pk6176838@gmail.com>
2024-08-04 06:33:55 -0700

    Signup form

---
+++ login.js
@@ -0,0 +1,22 @@
+const loginButton = document.querySelector('.btn-worker>button'); 
+const signupButton = document.querySelector('.btn-worker-seeker>button'); 
+const btnContainer = document.querySelector('.btn-container');
+const loginForm = document.querySelector('.login-form'); 
+const signupForm = document.querySelector('.signup-form'); 
+
+
+// loginButton.onclick = e => {
+//     // disapperar the control btn
+//     btnContainer.style.display = 'none';
+    
+//     // make login-form apperar 
+//     loginForm.style.left = '0px'; 
+// }
+
+// signupButton.onclick = e => {
+//     // disappear the control btn
+//     btnContainer.style.display = 'none'; 
+
+//     // make signup-form appear 
+//     signupForm.style.left = '0px'; 
+// }
