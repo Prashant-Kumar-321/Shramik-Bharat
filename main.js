40ba1f199399c036ceebfdc5ce588c0b3aec896e
pk123 <pk6176838@gmail.com>
2024-08-05 15:33:27 +0530

    Link register.html file with register button

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
-import './style.css'
+/* Open register.html file on click of register button  */
+
+const registerButton = document.querySelector('.register-btn'); 
+registerButton.onclick = e => {
+  window.location.href = './register.html'; 
+}
